25d35b2089dbebbd57710be79b82ad7dacd04ddc
Francesco <francesco12382@gmail.com>
2020-02-15 07:38:49 +0800

    commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-windows.alert("alert")
+
+
+
+function getMax() {
+  var num;
+  var max = prompt("Massimo numero", "300");
+  if (max == null || max == "") {
+    txt = "User cancelled the prompt.";
+  } else {
+    num = Number(max);
+    $("#numWrapper").removeClass("invis")
+    $("#startButton").addClass("invis")
+    raffle(num);
+  }
+}
+
+function raffle(max) {
+  var element =  document.getElementById('num')
+  var i = 0;
+  var chosen = getRandomIntInclusive(1, max)
+
+  var inv = setInterval(count, 3000 / 100)
+
+  console.log(chosen);
+
+  function count() {
+    if (i < chosen)
+      element.innerHTML = ++i;
+    else
+      clearInterval(inv);
+  }
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
