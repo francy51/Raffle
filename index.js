4ca1aec5aa21fa34a2b3a543ae44587e4b2abfea
Francesco <francesco12382@gmail.com>
2020-02-22 21:47:40 +0800

    reset

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 
-
+var num;
+var max = prompt("Massimo numero", "300");
+if (max == null || max == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  num = Number(max);
+}
 
 function getMax() {
-  var num;
-  var max = prompt("Massimo numero", "300");
-  if (max == null || max == "") {
-    txt = "User cancelled the prompt.";
-  } else {
-    num = Number(max);
-    $("#numWrapper").removeClass("invis")
-    $("#startButton").addClass("invis")
-    raffle(num);
-  }
+
 }
 
 function raffle(max) {
   var element =  document.getElementById('num')
   var i = 0;
-  var chosen = getRandomIntInclusive(1, max)
+  var chosen = getRandomIntInclusive(144, max)
 
   var inv = setInterval(count, 3000 / 100)
 
