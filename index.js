6b318a91a11124b943d94ee16c57512a80af33e8
francy51 <francesco12382@gmail.com>
2022-04-06 18:17:10 +0200

    updated random

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function getMax() {
 function raffle() {
   var element =  document.getElementById('num')
   var i = 0;
-  var chosen = getRandomIntInclusive(0, max)
+  var chosen = Math.random(0, max)
 
   var inv = setInterval(count, 3000 / 100)
 
@@ -26,10 +26,4 @@ function raffle() {
     else
       clearInterval(inv);
   }
-}
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
